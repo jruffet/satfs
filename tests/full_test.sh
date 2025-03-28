2c5bbb820dde00e11252e4af39f6c26610b6ded3
Jérémy Ruffet <sat@airnux.fr>
2025-03-28 22:37:52 +0100

    chore: better error handling

--- tests/full_test.sh
+++ tests/full_test.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+set -euo pipefail
+
 BOX_PREFIX="satfs-debian"
-
-# Return 0 if all tests passed
 keep_vagrant=false
-[ "$1" == "--keep-vagrant" ] && keep_vagrant=true
 
+cleanup() {
+    if [ $keep_vagrant == false ]; then
+        vagrant destroy -f
+    fi
+    exit 1
+}
+trap 'cleanup' EXIT
+
+### Start ###
 cd $(dirname $0)
+[ "${1:-}" == "--keep-vagrant" ] && keep_vagrant=true
 
 if [ $keep_vagrant == false ]; then
     cd vagrant
@@ -13,22 +22,18 @@ if [ $keep_vagrant == false ]; then
     cd ..
 fi
 
-# Run pytest locally + testinfra on vagrant for full test + remote tests "local to the vagrant box"
 echo "[+] local pytest"
-pytest -v && \
-cd vagrant && \
+pytest -v
+
+cd vagrant
+
 for version in bookworm64 testing64; do
-    box_name="${BOX_PREFIX}-${version}" && \
-    echo "[+] remote pytest ($box_name)" && \
-    vagrant ssh $box_name -c "sudo pytest /vagrant/tests/vagrant/remote/ -v --noconftest -p no:cacheprovider --forked" && \
-    echo "[+] testinfra ($box_name)" && \
+    box_name="${BOX_PREFIX}-${version}"
+    echo "[+] remote pytest ($box_name)"
+    vagrant ssh $box_name -c "sudo pytest /vagrant/tests/vagrant/remote/ -v --noconftest -p no:cacheprovider --forked"
+    echo "[+] testinfra ($box_name)"
     pytest -v --hosts=$box_name --ssh-config=<(vagrant ssh-config $box_name)
 done
 
-status=$?
-
-if [ $keep_vagrant == false ]; then
-    vagrant destroy -f
-fi
-
-exit $status
+# Reached only if no test failed
+exit 0
